295a04332465545ea024190e19aaa4757ff1dec1
artmenegon <162697347+artmenegon@users.noreply.github.com>
2024-10-03 07:49:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,19 +6,47 @@ const caixaResultado = document.querySelector('.caixa-resultado');
 const perguntas = [//abre a lista de objetos (itens)
     {//abre o item
         enunciado:"Você acha que a Inteligência Artificial pode ser prejudicial aos seres humanos?",
-        alternativas:[ "Sim","Não"]
+        alternativas:[{
+
+             texto: "Sim",
+             afirmação: "Não temos controle quanto a isso"
+       },
+       {
+             texto: "Não",
+             afirmação: "A utilidade e facilidade nos ajuda a evoluir"
+       }    
+        ]
     },
     {
         enunciado:"Você já assistiu o filme Eu, Robô?",
-        alternativas:[ "Sim","Não"]
+        alternativas:[ {
+
+            texto: "Sim",
+            afirmação: "Já assisti"
+      },
+      {
+            texto: "Não",
+            afirmação: "Nunca assisti"
+      }    
+       ]
     },
     {
-        enunciado:"Você acha que o filme e encaixa na realidade?",
-        alternativas:[ "Sim","Não"]
+        enunciado:"Você acha que o filme se encaixa na realidade?",
+        alternativas:[ {
+
+            texto: "Sim",
+            afirmação: "Nosso mundo pode se transformar no caos visto"
+      },
+      {
+            texto: "Não",
+            afirmação: "É uma realidade muito distante e utópica"
+      }    
+       ]
     }
 ]
 let posicao = 0
 let perguntaAtual;  
+let 
 
 function mostraPergunta(){
     perguntaAtual = perguntas[posicao];
@@ -28,7 +56,11 @@ function mostraPergunta(){
 function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativa){
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", function(){
+            posicao++;
+            mostraPergunta();
+        })
         caixaAlternativa.appendChild(botaoAlternativas);    
 
     }
